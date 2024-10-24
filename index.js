126d8acc3e98016b235f4bc7d8d84f1b40bd3f4d
Alexandre Ricardo <alexandre.n314159@gmail.com>
2024-10-23 21:42:28 -0300

    Arquivos estáticos públicos.

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 3030;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello, World!" });
+app.use("/", express.static("public"));
+
+app.get("/dev", (req, res) => {
+  res.json({ message: "Hello, World!" }); 
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+  console.log(`[Server]: Server is running on http://localhost:${port}/`);
 });
